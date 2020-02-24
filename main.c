08bbe03e8c77d76880276794715fc921ecdbd5c2
Vladislav Vinogradov <optimistforever98@mail.ru>
2020-02-24 17:18:35 +0300

    Use proper grammar.

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 
 int main(int argc, char **argv)
 {
-printf("Hello world!\n");
-char name[255];
 printf("Enter your name: ");
 fgets(name, 255, stdin);
-printf("length = %d\n", strlen(name)); /* debug line */
-name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-printf("Hello %s!\n", name);
+name[strlen(name)-1] = '\0';
+
+printf("Hello, %s!\n", name);
 return 0;
 }
